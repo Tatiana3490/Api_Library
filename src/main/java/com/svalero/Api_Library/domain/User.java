76bce7296dfbe25d7d8a51cd6070c1c5cbedde76
Tatiana3490 <tatiana.alcubilla.m@gmail.com>
2025-05-28 20:47:30 +0200

    correciones en el codigo de User por validaciones correctas

--- src/main/java/com/svalero/Api_Library/domain/User.java
+++ src/main/java/com/svalero/Api_Library/domain/User.java
@@ -1,42 +1,49 @@
 package com.svalero.Api_Library.domain;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity(name = "User")
-@Table(name="users")
+@Table(name = "users")
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotNull(message = "Name is required")
+
+    @NotBlank(message = "Name is required")
     @Column(nullable = false, unique = true)
     private String name;
-    @NotNull(message = "Username is required")
+
+    @NotBlank(message = "Username is required")
     @Column(nullable = false, unique = true)
     private String username;
-    @NotNull(message = "Email is required")
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Invalid email format")
     @Column(nullable = false, unique = true)
     private String email;
-    @Column(nullable = false, unique = true)
+
+    @NotBlank(message = "Password is required")
     @Size(min = 8, max = 30, message = "Password must be between 8 and 30 characters")
+    @Column(nullable = false, unique = true)
     private String password;
-    @Column(name = "creation_date")
+
+    @NotNull(message = "Creation date is required")
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
+    @Column(name = "creation_date", nullable = false)
     private LocalDate creationDate;
-    private Boolean active = true;
 
-
+    @NotNull(message = "Active status is required")
+    @Column(nullable = false)
+    private Boolean active;
 }
